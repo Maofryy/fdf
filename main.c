331bd2ea51b523f0f77ef6b49fdd067387fe4e93
Maori BENHASSINE <mbenhass@e2r12p19.42.fr>
2019-10-28 18:29:37 +0100

    First line drawn, yikes

--- main.c
+++ main.c
@@ -6,12 +6,34 @@
 /*   By: mbenhass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 12:57:54 by mbenhass          #+#    #+#             */
-/*   Updated: 2019/10/28 18:14:32 by mbenhass         ###   ########.fr       */
+/*   Updated: 2019/10/28 18:28:43 by mbenhass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 #include "mlx.h"
+
+int	draw_line(void	*mlx_ptr, void *win_ptr, int x1, int y1, int x2, int y2)
+{
+	int dx;
+	int dy;
+	int x;
+	int y;
+	int ret;
+
+	dx = x2 - x1;
+	dy = y2 - y1;
+	x = x1;
+	while (x != x2)
+	{
+		y = y1 + dy * (x - x1) / dx;
+		ret = mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xffffff);
+		ft_putnbr(x);
+		ft_putchar('\n');
+		x++;
+	}
+	return (1);
+}
 
 int main(int ac, char **av)
 {
@@ -19,7 +41,7 @@ int main(int ac, char **av)
 	//t_fdf fdf;
 	void *mlx_ptr;
 	void *win_ptr;
-	int ret;
+	int draw_ret;
 
 	//fdf_init(fdf);
 	if (ac != 2)
@@ -29,8 +51,8 @@ int main(int ac, char **av)
 	}
 	mlx_ptr = mlx_init();
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, av[1]);
-	ret = mlx_string_put(mlx_ptr, win_ptr, 10, 10, 0xffffffff, av[1]);
-
+	draw_ret = draw_line(mlx_ptr, win_ptr, 1, 1, 500, 200);
+	ft_putstr("Finished drawing");
 	mlx_loop(mlx_ptr);
 	//fdf.av = ft_strdup(av[1]);
 	//map = parse_file(fdf);
